fb1ef8d8b31361e33dda3fc809a3f76e56ee5ede
Apsara22 <paneruapsara831ap@gmail.com>
2026-07-16 15:50:02 +0545

    commit the project first

--- frontend/src/api/userApi.ts
+++ frontend/src/api/userApi.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://ecommerce-grocery-gamma.vercel.app";
 
 // USER API
 const userAPI = axios.create({
